09e4f0ef010322cd552dfebdfdbbc59d8a36a6e4
선혁준 <tjsrmatn@gmail.com>
2019-10-03 06:51:20 +0900

    메뉴관리 작업

---
+++ eatgo-api/src/main/java/kr/co/seonhyeokjun/eatgo/application/MenuItemService.java
@@ -0,0 +1,26 @@
+package kr.co.seonhyeokjun.eatgo.application;
+
+import kr.co.seonhyeokjun.eatgo.domain.MenuItem;
+import kr.co.seonhyeokjun.eatgo.domain.MenuItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+    private MenuItemRepository menuItemRepository;
+
+    @Autowired
+    public MenuItemService(MenuItemRepository menuItemRepository) {
+        this.menuItemRepository = menuItemRepository;
+    }
+
+    public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems) {
+        for (MenuItem menuItem : menuItems){
+            menuItem.setRestaurantId(restaurantId);
+            menuItemRepository.save(menuItem);
+        }
+    }
+}
