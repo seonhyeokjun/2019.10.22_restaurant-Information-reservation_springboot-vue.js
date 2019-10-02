0998bdb6b8999684161bd04dacfef9327780e383
선혁준 <tjsrmatn@gmail.com>
2019-10-03 02:11:06 +0900

    유효성 검증 테스트

--- eatgo-api/src/main/java/kr/co/seonhyeokjun/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/seonhyeokjun/eatgo/domain/Restaurant.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
@@ -22,8 +23,10 @@ public class Restaurant {
     @Setter
     private Long id;
 
+    @NotEmpty
     private String name;
 
+    @NotEmpty
     private String address;
 
     @Transient
