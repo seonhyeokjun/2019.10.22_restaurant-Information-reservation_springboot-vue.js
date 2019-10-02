1317a9e5f0a336f7102733c19e997a937e9aeb58
선혁준 <tjsrmatn@gmail.com>
2019-10-02 17:32:29 +0900

    JPA적용

--- eatgo-api/src/main/java/kr/co/seonhyeokjun/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/seonhyeokjun/eatgo/domain/Restaurant.java
@@ -1,14 +1,24 @@
 package kr.co.seonhyeokjun.eatgo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 
+@Entity
 public class Restaurant {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
     private String name;
     private String address;
+
+    @Transient
     private List<MenuItem> menuItems = new ArrayList<MenuItem>();
 
     public Restaurant() {
--- eatgo-api/src/main/java/kr/co/seonhyeokjun/eatgo/domain/RestaurantRepository.java
+++ eatgo-api/src/main/java/kr/co/seonhyeokjun/eatgo/domain/RestaurantRepository.java
@@ -1,13 +1,14 @@
 package kr.co.seonhyeokjun.eatgo.domain;
 
-
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface RestaurantRepository {
+public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
     List<Restaurant> findAll();
 
-    Restaurant findById(Long id);
+    Optional<Restaurant> findById(Long id);
 
     Restaurant save(Restaurant restaurant);
 }
