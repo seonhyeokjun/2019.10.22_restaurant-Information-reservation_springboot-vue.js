c9aafec526079ed49119aac2acfcee9a908f9e26
선혁준 <tjsrmatn@gmail.com>
2019-10-07 17:29:04 +0900

    리뷰기능 추가

---
+++ eatgo-api/src/main/java/kr/co/seonhyeokjun/eatgo/application/ReviewService.java
@@ -0,0 +1,11 @@
+package kr.co.seonhyeokjun.eatgo.application;
+
+import kr.co.seonhyeokjun.eatgo.domain.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    public void addReview(Review review) {
+    }
+}
---
+++ eatgo-api/src/main/java/kr/co/seonhyeokjun/eatgo/domain/Review.java
@@ -0,0 +1,20 @@
+package kr.co.seonhyeokjun.eatgo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue
+    Long id;
+}
